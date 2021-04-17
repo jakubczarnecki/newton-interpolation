45ccc2b39af3d915dbe7d974c46400723c616f78
229935 <229935@edu.p.lodz.pl>
2021-04-17 17:37:06 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    print("\nMetody numeryczne - zadanie 3\n")
+    print("Wariant 5 - interpolacja Newtona dla nierównych odstępów argumentu")
+    print("Autorzy - Oskar Kurczewski i Jakub Czarnecki")
