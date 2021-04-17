307dc6aa583a279a9df40ae5b594446c24dc29eb
jc <kczarnecok@wp.pl>
2021-04-17 20:42:27 +0200

    gui w mainie v alpha

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import functions as f
+import numpy as np
 from horner import horner
 
 if __name__ == '__main__':
     print("\nMetody numeryczne - zadanie 3\n")
     print("Wariant 5 - interpolacja Newtona dla nierównych odstępów argumentu")
     print("Autorzy - Oskar Kurczewski i Jakub Czarnecki")
+
+    left, right = np.double
+    number = np.uint
+
+    print('Podaj dolna granice interpolacji:')
+    input(left)
+    print('Podaj dolna granice interpolacji:')
+    input(right)
+    print('Podaj liczbe wezlow:')
+    input(number)
+
+
 
